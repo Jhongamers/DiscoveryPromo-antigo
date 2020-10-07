96b1a2eac4d9853d58b7f4a519d2832144525494
jhogamers <jonverdao11@gmail.com>
2020-10-07 15:41:49 -0300

    update design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 import './styles.css';
-const Footer = () => <div className="Footer"><a target="_blank" rel="noopener noreferrer" href="https://bit.ly/3jP9MCE" >apoie-nos</a><p>Copyrights © Discovery Promo 2020. Todos os direitos reservados.</p></div>
+const Footer = () => <div className="Footer"><div className="apo"><a target="_blank" rel="noopener noreferrer" href="https://bit.ly/3jP9MCE" >apoie-nos</a> </div><div className="cop"><p>Copyrights © Discovery Promo 2020. Todos os direitos reservados.</p></div></div>
 
 export default Footer;
