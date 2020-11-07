64e01bf4ecbd7fe261b5c53a756809248116f008
jhogamers <jonverdao11@gmail.com>
2020-11-07 18:28:25 -0300

    update v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
-<<<<<<< HEAD
 import Main from './pages/main';
-=======
-import Main   from './pages/main';
-import Footer from './components/Footer';
->>>>>>> 96b1a2eac4d9853d58b7f4a519d2832144525494
 import './styles.css';
 function App() {
   return (
     <div className="App">
     <Header />
-<<<<<<< HEAD
     <Main />
-=======
-    {/* <Left /> */}
-    <Main />
-    <Footer />
->>>>>>> 96b1a2eac4d9853d58b7f4a519d2832144525494
     </div>
   );
 }
