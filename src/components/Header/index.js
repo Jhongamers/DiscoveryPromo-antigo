e09a7b38a23d3fe637205d6316b76bb821ef880c
jhogamers <jonverdao11@gmail.com>
2020-11-07 18:26:26 -0300

    update v1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './styles.css';
-<<<<<<< HEAD
 const Header = () => <header id="main-header"><div className="icon">Discovery Promo</div><div className="searchbar"><input type="text" placeholder="Search"></input><button>Search</button></div></header>
-=======
-const Header = () => <header id="main-header"><a rel="noopener noreferrer" href="/"><div className="icon"><p>Discovery Promo</p></div></a><div className="container"><input type="text" placeholder="Search"></input><button>Search</button></div></header>
->>>>>>> 96b1a2eac4d9853d58b7f4a519d2832144525494
 
 export default Header;
